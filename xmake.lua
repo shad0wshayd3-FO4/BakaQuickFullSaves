c752b2f1521296a98932a56d2cf21c78daa8dc81
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2026-08-30 04:41:53 -0600

    revert: release impl

--- xmake.lua
+++ xmake.lua
@@ -30,6 +30,3 @@ target("BakaQuickFullSaves")
 
     -- add extra files
     add_extrafiles(".clang-format")
-
-    -- add install files
-    add_installfiles("res/MCM/**.*", { prefixdir = "MCM/config/BakaQuickFullSaves" })
